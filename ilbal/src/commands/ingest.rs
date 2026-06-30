9be0e11788b35be58865519d397174545334056d
chikaj <ncurrit@gmail.com>
2026-06-29 21:45:06 -0500

    ingest is good enough for now!

--- ilbal/src/commands/ingest.rs
+++ ilbal/src/commands/ingest.rs
@@ -1,11 +1,12 @@
 use clap::ArgMatches;
+use dialoguer::Select;
 use owo_colors::OwoColorize;
 use std::io::Write;
 use std::process::Command;
 
 const DOCKER_IMAGE: &str = "ilbal-postgresql-loaders-0.1.0:latest";
 
-pub fn run(matches: &ArgMatches) -> anyhow::Result<()> {
+pub fn run(cmd: &mut clap::Command, matches: &ArgMatches) -> anyhow::Result<()> {
     let cwd = std::env::current_dir()
         .expect("Could not determine current directory")
         .to_string_lossy()
@@ -54,10 +55,80 @@ pub fn run(matches: &ArgMatches) -> anyhow::Result<()> {
             }
         }
         None => {
-            eprintln!("Usage: ilbal ingest <binary> [args...]");
+            cmd.print_help()?;
+            println!();
             println!("{}", "Available binaries:".yellow());
-            // std::io::stdout().write_all(&output.stdout)?;
+            Select::new()
+                .with_prompt("Press Enter to close")
+                .items(BINARIES)
+                .max_length(10)
+                .interact()?;
         }
     }
     Ok(())
 }
+
+const BINARIES: &[&str] = &[
+    "catcsv",
+    "cs2cs",
+    "dbcrossbar",
+    "fixed2csv",
+    "gdal",
+    "gdal-config",
+    "gdal2tiles",
+    "gdal2xyz",
+    "gdal_calc",
+    "gdal_contour",
+    "gdal_create",
+    "gdal_edit",
+    "gdal_fillnodata",
+    "gdal_footprint",
+    "gdal_grid",
+    "gdal_merge",
+    "gdal_pansharpen",
+    "gdal_polygonize",
+    "gdal_proximity",
+    "gdal_rasterize",
+    "gdal_retile",
+    "gdal_sieve",
+    "gdal_translate",
+    "gdal_viewshed",
+    "gdaladdo",
+    "gdalattachpct",
+    "gdalbuildvrt",
+    "gdalcompare",
+    "gdaldem",
+    "gdalenhance",
+    "gdalinfo",
+    "gdallocationinfo",
+    "gdalmanage",
+    "gdalmdiminfo",
+    "gdalmdimtranslate",
+    "gdalmove",
+    "gdalsrsinfo",
+    "gdaltindex",
+    "gdaltransform",
+    "gdalwarp",
+    "geochunk",
+    "geocode-csv",
+    "geod",
+    "gie",
+    "gnmanalyse",
+    "gnmmanage",
+    "hashcsv",
+    "invgeod",
+    "invproj",
+    "nearblack",
+    "ogr2ogr",
+    "ogr_layer_algebra",
+    "ogrinfo",
+    "ogrlineref",
+    "ogrmerge",
+    "ogrtindex",
+    "pct2rgb",
+    "proj",
+    "projinfo",
+    "projsync",
+    "rgb2pct",
+    "scrubcsv",
+];
--- ilbal/src/main.rs
+++ ilbal/src/main.rs
@@ -3,6 +3,36 @@ use indoc::indoc;
 mod commands;
 
 fn main() -> anyhow::Result<()> {
+    const INGEST_LONG_HELP: &str = indoc!(
+        "Command line arguments unique to each binary
+
+        All binaries come from the following sources.
+        Details about each binary can be found at the respective link.
+
+        https://www.dbcrossbar.org
+        https://www.gdal.org
+        https://www.pgferry.com
+        https://github.com/faradayio/csv-tools."
+    );
+
+    let ingest_cmd = Command::new("ingest")
+        .about("Convert, transform, and load data into ilbal database")
+        .arg(
+            Arg::new("save")
+                .long("save")
+                .short('s')
+                .num_args(0..=1)
+                .value_name("data/output.csv")
+                .help("Save the output to a file, if applicable"),
+        )
+        .arg(
+            Arg::new("args")
+                .num_args(0..)
+                .trailing_var_arg(true)
+                .help("Command line arguments unique to each binary")
+                .long_help(INGEST_LONG_HELP),
+        );
+
     // ***** Collect command line arguments *****
     let match_result = command!()
         .about(indoc!(
@@ -17,44 +47,20 @@ fn main() -> anyhow::Result<()> {
         .subcommand(
             Command::new("pgbranch")
                 .about("Git-like actions on local ilbal database")
-                // .disable_help_flag(true)
                 .arg(Arg::new("args").num_args(0..).trailing_var_arg(true)),
         )
-        // .disable_help_flag(true)
         .subcommand(Command::new("pgroll").about("Push database changes"))
         .subcommand(Command::new("pull").about("Pull database from ilbal cloud"))
-        .subcommand(
-            Command::new("ingest")
-                .about("Convert, transform, and load data into ilbal database")
-                .arg(
-                    Arg::new("save")
-                        .long("save")
-                        .short('s')
-                        .num_args(0..=1)
-                        .value_name("data/output.csv")
-                        .help("Save the output to a file"),
-                )
-                .arg(
-                    Arg::new("args")
-                        .num_args(0..)
-                        .trailing_var_arg(true)
-                        .help("Command line arguments unique to each binary"),
-                ),
-        )
+        .subcommand(ingest_cmd.clone())
         .get_matches();
 
     // ***** Process command line arguments *****
+    let mut ingest_cmd = ingest_cmd;
     match match_result.subcommand_name() {
-        // Some("init") => commands::init::run(&match_result),
-        // Some("start") => commands::start::run(),
-        // Some("stop") => commands::stop::run(),
-        // Some("status") => commands::status::run(),
-        // Some("pgbranch") => {
-        //     commands::pgbranch::run(match_result.subcommand_matches("pgbranch").unwrap())
-        // }
-        // Some("pgroll") => commands::pgroll::run(match_result.subcommand_matches("pgroll").unwrap()),
-        // Some("pull") => commands::pull::run(),
-        Some("ingest") => commands::ingest::run(match_result.subcommand_matches("ingest").unwrap()),
+        Some("ingest") => commands::ingest::run(
+            &mut ingest_cmd,
+            match_result.subcommand_matches("ingest").unwrap(),
+        ),
         _ => Ok(()),
     }
 }
